4e5637d4890d83cd054025a963209bfc5ce2eb23
gmwu185 <gmwu185@gmail.com>
2020-05-23 01:10:24 +0800

    完成 Modal-add-full 內容、拆分 jade 區塊依元件與頁面分類、BS 4 表單變數依 XD 設計稿設定

--- source/assets/js/custom.js
+++ source/assets/js/custom.js
@@ -2,7 +2,15 @@ console.log('js all');
 
 $(function () {
   console.log('jquery run');
-  // alert('jQuery run')
+  $('.js-call-coverPanel').click( function (e) {
+    e.preventDefault();
+    // console.log('.js-call-coverPanel on click');
+    $(".js-coverPanel-section").addClass('active');
+  });
+  $('.js-close-coverPanel').click( function (e) {
+    e.preventDefault();
+    $(".js-coverPanel-section").removeClass('active');
+  })
 })
 
 
